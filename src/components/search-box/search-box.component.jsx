9658df9354b64bbc3c086c0c493feea20ace7c42
Siddharth <siddharthkochkar@gmail.com>
2020-09-28 01:57:02 +0530

    initial commit

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './search-box.styles.css';
+
+export const SearchBox = ({placeholder, handleChange})=> (
+
+    <input 
+    type="search" 
+    placeholder={placeholder} 
+    className='search'
+    onChange={ handleChange }
+    >
+
+    </input>
+);
