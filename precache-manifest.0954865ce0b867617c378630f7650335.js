eb14772ae8d8428aec441ea8a1ea48e9e85e32bf
Siddharth <siddharthkochkar@gmail.com>
2020-09-28 02:06:02 +0530

    Updates

---
+++ precache-manifest.0954865ce0b867617c378630f7650335.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81f1bc1354747903c5e6cb63525bdab6",
+    "url": "/monsters-index/index.html"
+  },
+  {
+    "revision": "622d674e0b64aea3f850",
+    "url": "/monsters-index/static/css/main.333e3ab6.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monsters-index/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-index/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "622d674e0b64aea3f850",
+    "url": "/monsters-index/static/js/main.f465ba7e.chunk.js"
+  },
+  {
+    "revision": "45cef1979b640c5fb33d",
+    "url": "/monsters-index/static/js/runtime-main.bda7c949.js"
+  }
+]);
